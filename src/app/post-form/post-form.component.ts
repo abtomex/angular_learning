cab48ec369032d7a0a8b197c3cf5d378ca538f05
dchechumaev <dmitriy.chechumaev@v6services.ru>
2021-07-29 19:24:36 +0300

    Доступ до HTML элементов

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {Post} from '../app.component';
 
 @Component({
@@ -8,7 +8,10 @@ import {Post} from '../app.component';
 })
 export class PostFormComponent implements OnInit {
 
+
+  // tslint:disable-next-line:no-output-on-prefix
   @Output() onAdd: EventEmitter<Post> = new EventEmitter<Post>();
+  @ViewChild('titleInput', {static: false}) inputRef: ElementRef;
 
   title = '';
   text = '';
@@ -18,7 +21,6 @@ export class PostFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // tslint:disable-next-line:typedef
   addPost() {
     if (this.text.trim() && this.title.trim()){
       const post: Post = {
@@ -29,5 +31,11 @@ export class PostFormComponent implements OnInit {
       // console.log('New post: ', post);
       this.title = this.text = '';
     }
+  }
+
+
+  focusTitle() {
+    this.inputRef.nativeElement.focus();
+    // console.log(this.inputRef);
   }
 }
